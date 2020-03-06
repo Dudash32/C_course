c4daeb5c0fd33b277ad038ceb1f6c84b5842dbfe
Michael Mulderink <michaelmulderink@Michaels-MBP-2.lan>
2020-03-05 23:25:25 -0600

    pSet1 is looking Great

--- pSet1.c
+++ pSet1.c
@@ -1,7 +1,9 @@
+// Mike M -> This code is looking Great!
+
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) 
+int main(void)
 {
     // declare name var
     string name = get_string("what is your name?\n");
