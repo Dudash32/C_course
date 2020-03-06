b6c9117e840a2ce6986a7958a43a99438bae27e7
John Dudash <dudash32@gmail.com>
2020-03-05 21:32:35 -0600

    first cs50 commit

---
+++ pSet1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) 
+{
+    // declare name var
+    string name = get_string("what is your name?\n");
+
+    // print out name var with string interpilation
+    printf("hello, %s\n", name);
+}
